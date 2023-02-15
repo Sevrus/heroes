b47b37d11e1c7823e42d1947bd8d4f81155e42b5
Sevrus <book-n-troll@nordnet.fr>
2023-02-15 20:42:16 +0100

    Added content to the character and scenario pages

--- src/routes/Personnage.jsx
+++ src/routes/Personnage.jsx
@@ -1,7 +1,25 @@
+import avatar from '../assets/img/avatar.png'
+
 const Personnage = () => {
   return(
 <section>
     <h2>CRÉATION DE PERSONNAGE</h2>
+    <div>
+      <img src={avatar} alt="Image de votre avatar"/>
+      <div>
+
+      </div>
+    </div>
+    <div>
+      <article>
+        <h3></h3>
+        <p></p>
+      </article>
+      <article>
+        <h3></h3>
+        <p></p>
+      </article>
+    </div>
 </section>
   )
 }
--- src/routes/Scenario.jsx
+++ src/routes/Scenario.jsx
@@ -2,6 +2,35 @@ const Scenario = () => {
   return(
       <section>
           <h2>Choisissez Votre Scénario</h2>
+          <div>
+              <div>
+                  <h3>L'aventurier solitaire</h3>
+                  <ol>
+                      <li>Les plaines de Quatrevent</li>
+                      <li>Le serviteur des âmes</li>
+                      <li>Les tours du silence</li>
+                      <li>Étranges rencontres</li>
+                      <li>L'artefact perdu</li>
+                      <li>"Au sanglier rigolard"</li>
+                      <li>La croisée des chemins</li>
+                      <li>Le château d'Ambroisie</li>
+                  </ol>
+              </div>
+              <div>
+                  <h3>La grande épopée</h3>
+                  <ol>
+                      <li>Le chevalier de Capestan</li>
+                      <li>Le tabouret de transport</li>
+                      <li>La quête du serpent du lac</li>
+                      <li>De sa propre initiative</li>
+                      <li>Chevalièrisation</li>
+                      <li>Chante Sloubi</li>
+                      <li>Naindice</li>
+                      <li>La pierre incandescente</li>
+                      <li>L’élan</li>
+                  </ol>
+              </div>
+          </div>
       </section>
   )
 }
